259ed448b7d625c87fb6aa08f677f052de393e40
Sam <49291807+EurusSharpness@users.noreply.github.com>
2021-10-21 06:34:57 +0300

    firebase connected

--- app/src/main/java/com/example/goodstuff/MainActivity.java
+++ app/src/main/java/com/example/goodstuff/MainActivity.java
@@ -1,38 +1,79 @@
 package com.example.goodstuff;
 
-import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Application;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 import android.text.InputType;
 
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
 public class MainActivity extends AppCompatActivity {
 
 
     TextView textView;
-    int currentHours = 0;
+    long currentHours = 0;
+    Map<String, Date> currentDates = null;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private static class Collections {
+        public static String USERS = "users";
+    }
+
+    private static class Users {
+        public static String Sam = "Sam";
+    }
+
+    private static class Fields {
+        public static String Dates = "Dates";
+        public static String TotalHours = "Total Hours";
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
         setContentView(R.layout.activity_main);
+        textView = this.findViewById(R.id.TopTextID);
 
+        db.collection(Collections.USERS).document(Users.Sam).get().addOnCompleteListener((tasl)-> {
+            DocumentSnapshot value = tasl.getResult();
 
+            assert value != null;
+            Map<String, Object> qs = value.getData();
+            assert qs != null;
+            assert qs.get(Fields.TotalHours) != null;
+            assert qs.get(Fields.Dates) != null;
+            long totalHours = (long) qs.get(Fields.TotalHours);
+            ArrayList<Map<String, Long>> dates = (ArrayList<Map<String, Long>>) qs.get(Fields.Dates);
+            currentDates = null;
+            assert dates != null;
+            System.out.println(dates.get(0).get("hours"));
+           /* dates.forEach((s, timestamp) -> {
+                System.out.println(timestamp);
+            });*/
+            System.out.println(totalHours);
+            currentHours = totalHours;
+            textView.setText(String.valueOf(currentHours));
+        });
         (this.findViewById(R.id.fab)).setOnClickListener(view -> {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("Enter Number of hours to add");
@@ -42,11 +83,18 @@ public class MainActivity extends AppCompatActivity {
             builder.setView(input);
 
             builder.setPositiveButton("OK", (dialog, which) -> {
-                textView = this.findViewById(R.id.TopTextID);
                 String value = input.getText().toString();
                 System.out.println(value);
                 currentHours += Integer.parseInt(value);
                 textView.setText(String.valueOf(currentHours));
+                db.collection(Collections.USERS).document(Users.Sam).update(Fields.TotalHours, currentHours).addOnCompleteListener(task -> {
+                    System.out.println("Updated result " + task.getResult());
+                });
+                /*currentDates.add(new Date());
+
+                db.collection(Collections.USERS).document(Users.Sam).update(Fields.Dates, currentDates).addOnCompleteListener(task -> {
+                    System.out.println("Updated result " + task.getResult());
+                });*/
             });
             builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
 
